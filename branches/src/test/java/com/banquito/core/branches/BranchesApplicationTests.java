67935e759ea58e82ede24be55d9f508479e70223
Daniel Gavilanes <dagavilanes2@espe.edu.ec>
2023-08-25 21:17:48 -0500

    Initial commit

---
+++ branches/src/test/java/com/banquito/core/branches/BranchesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.banquito.core.branches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchesApplicationTests {
+
+
+
+}
+
